00343397d188eb0829db2048dd29882f56585a28
Megan Short <mshov@allstate.com>
2017-04-05 09:15:20 -0400

    update gradle fiel

---
+++ src/test/java/com/compozed/acceptance/LoginAcceptanceTest.java
@@ -0,0 +1,14 @@
+//package com.compozed.acceptance;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.fluentlenium.core.hook.wait.Wait;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@Wait
+//public class LoginAcceptanceTest {
+//
+//
+//}
